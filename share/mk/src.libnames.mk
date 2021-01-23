22a3cd1972c3ef2e93657ee21596d40cb0be5458
Bernard Spil <brnrd@FreeBSD.org>
2021-01-23 17:58:10 +0000

    LibreBSD: Update FreeBSD build framework for LibreSSL

--- share/mk/src.libnames.mk
+++ share/mk/src.libnames.mk
@@ -205,6 +205,7 @@ _LIBRARIES=	\
 		tacplus \
 		termcapw \
 		tpool \
+		tls \
 		ufs \
 		ugidfw \
 		ulog \
@@ -289,6 +290,7 @@ _DP_archive+=	md
 .endif
 _DP_sqlite3=	pthread
 _DP_ssl=	crypto
+_DP_tls=	crypto ssl
 _DP_ssh=	crypto crypt z
 .if ${MK_LDNS} != "no"
 _DP_ssh+=	ldns
@@ -740,6 +742,7 @@ LIBSPLDIR=	${OBJTOP}/cddl/lib/libspl
 LIBSSHDIR=	${OBJTOP}/secure/lib/libssh
 LIBSSLDIR=	${OBJTOP}/secure/lib/libssl
 LIBTEKENDIR=	${OBJTOP}/sys/teken/libteken
+LIBTLSDIR=	${OBJTOP}/secure/lib/libtls
 LIBEGACYDIR=	${OBJTOP}/tools/build
 LIBLNDIR=	${OBJTOP}/usr.bin/lex/lib
 

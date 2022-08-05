fe2da0069a4b12690f764589a3f8014a4ec7bb9f
Bernard Spil <brnrd@FreeBSD.org>
2022-08-05 23:38:39 +0200

    crypto/heimdal: Update for LibreSSL 3.5

--- crypto/heimdal/lib/hx509/hxtool.c
+++ crypto/heimdal/lib/hx509/hxtool.c
@@ -1388,12 +1388,12 @@ info(void *opt, int argc, char **argv)
     {
 	const RSA_METHOD *m = RSA_get_default_method();
 	if (m != NULL)
-	    printf("rsa: %s\n", m->name);
+	    printf("rsa: %s\n", RSA_meth_get0_name(m));
     }
     {
 	const DH_METHOD *m = DH_get_default_method();
-	if (m != NULL)
-	    printf("dh: %s\n", m->name);
+/*	if (m != NULL)
+	    printf("dh: %s\n", m->name);*/
     }
 #ifdef HAVE_OPENSSL
     {
